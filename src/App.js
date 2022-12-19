f0d657dbcee88f40464111665027028b738067db
loki5657 <thadikamallalokesh7@gmail.com>
2022-12-19 14:15:07 +0530

    basic setup and layout setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-     
+      <nav id="sidebarMenu" className="collapse d-lg-block sidebar collapse">
+        <div className="list-group list-group-flush">
+          <a href="#" className="list-group-item textbg_color" >
+            <i className="textbg_color"></i><span> Dashboard</span>
+          </a>
+        </div>
+      </nav>
+      <div class="container box-shadow">
+        <div class="row">
+          <div class="col">
+            graph
+          </div>
+          <div class="col">
+            new registration
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            total No OF ppl
+          </div>
+          <div class="col">
+            Locations
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
